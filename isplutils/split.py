5abc168e4c08092509dc79072afafeb4d6e7a248
phamlinh2111 <k.linh21112003@gmail.com>
2025-04-05 13:35:40 +0700

    update

--- isplutils/split.py
+++ isplutils/split.py
@@ -57,12 +57,18 @@ def get_split_df(df: pd.DataFrame, dataset: str, split: str) -> pd.DataFrame:
         random_youtube_videos = np.random.permutation(
             df[(df['source'] == 'youtube') & (df['quality'] == crf)]['video'].unique())
         
-        train_orig = random_youtube_videos[:300]
-        val_orig = random_youtube_videos[300:300 + 100]
-        test_orig = random_youtube_videos[300 + 100:]
-        output_file = "/content/drive/MyDrive/KLTN/selected_youtube_videos.txt"
+        train_orig = random_youtube_videos[:720]
+        val_orig = random_youtube_videos[720:720 + 140]
+        test_orig = random_youtube_videos[720 + 140:]
+        output_file = "/content/drive/MyDrive/KLTN/selected_youtube_videos.txt"  
         np.savetxt(output_file, random_youtube_videos, fmt="%s")
         print(f"Danh sách video đã lấy được lưu vào: {output_file}")
+        output_file_1 = "/content/drive/MyDrive/KLTN/train_orig.txt"  
+        np.savetxt(output_file_1, train_orig, fmt="%s")
+        print(f"Danh sách video đã lấy được lưu vào: {output_file_1}")
+        output_file_2 = "/content/drive/MyDrive/KLTN/val_orig.txt"  
+        np.savetxt(output_file_2, val_orig, fmt="%s")
+        print(f"Danh sách video đã lấy được lưu vào: {output_file_2}")
         if split == 'train':
             split_df = pd.concat((df[df['original'].isin(train_orig)], df[df['video'].isin(train_orig)]), axis=0)
         elif split == 'val':
